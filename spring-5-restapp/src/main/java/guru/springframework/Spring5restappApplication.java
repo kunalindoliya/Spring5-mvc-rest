3bccee9f56d1429414ed42b33ce1c994100a2c4a
Kunal Indoliya <kunalindoliya@gmail.com>
2018-12-07 15:44:20 +0530

    Change to Multi Module Maven Project.

---
+++ spring-5-restapp/src/main/java/guru/springframework/Spring5restappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5restappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5restappApplication.class, args);
+    }
+}
